199eb00a36743bccbc781115dcbf29d6ed014d8c
Mohammad Shahanwaz <shahanwaz@bosscoderacademy.com>
2023-03-30 02:57:57 +0530

    Added new design in home page

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,20 @@
+import cn from '@/core/utils/cn';
 import React, { ReactNode } from 'react';
 
-export default function Button({ children }: Props) {
-  return <button className="p-4 bg-black text-white">{children}</button>;
+export default function Button({ children, className }: Props) {
+  return (
+    <button
+      className={cn(
+        'px-5 py-3 rounded-lg font-medium bg-black text-white ring-0 ring-gray-300 focus:outline-none focus:ring-4 hover:bg-gray-900',
+        className
+      )}
+    >
+      {children}
+    </button>
+  );
 }
 
 type Props = {
   children: ReactNode;
+  className?: string;
 };
