4808e3fd6b8e2b969e6434b30005abfdd249ce49
Mohammad Shahanwaz <shahanwaz@bosscoderacademy.com>
2023-03-31 02:15:06 +0530

    Added resume link in CTA

--- src/assets/svgs/icons/InstagramIcon.tsx
+++ src/assets/svgs/icons/InstagramIcon.tsx
@@ -4,10 +4,10 @@ export default function InstagramIcon(props: { className?: string }) {
       className={props.className}
       viewBox="0 0 24 24"
       stroke="currentColor"
-      stroke-width="2"
+      strokeWidth="2"
       fill="none"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     >
       <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
       <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
