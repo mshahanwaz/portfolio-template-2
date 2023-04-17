87ddcf426477609db9a0131745c6637b2e6a4cce
Mohammad Shahanwaz <shahanwaz@bosscoderacademy.com>
2023-04-17 22:37:27 +0530

    Added real projects in project section with an image of noto app

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -7,30 +7,51 @@ import Link from 'next/link';
 
 const PROJECTS = [
   {
-    title: 'Project 1',
-    brief:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title: 'Noto App',
+    description:
+      'A simple and minimal notes + todo taking application with CRUD functionality along with custom key bindings.',
     image: [
       'https://images.unsplash.com/photo-1508919801845-fc2ae1bc2a28?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1nfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
       'https://images.unsplash.com/photo-1508919801845-fc2ae1bc2a28?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1nfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
       'https://images.unsplash.com/photo-1508919801845-fc2ae1bc2a28?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1nfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
     ],
-    link: 'https://google.com',
-    github: 'https://github.com',
-    techs: ['Next.js', 'Tailwind CSS', 'TypeScript'],
+    link: 'https://noto-app.vercel.app/',
+    github: 'https://github.com/mshahanwaz/noto-app',
   },
   {
-    title: 'Project 1',
-    brief:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title: 'Portfolio v4',
+    description:
+      'Newest iteration of my simple, aesthetic and minimal personal portfolio website to showcase my work and experience.',
     image: [
       'https://images.unsplash.com/photo-1508919801845-fc2ae1bc2a28?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1nfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
       'https://images.unsplash.com/photo-1508919801845-fc2ae1bc2a28?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1nfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
       'https://images.unsplash.com/photo-1508919801845-fc2ae1bc2a28?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1nfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
     ],
-    link: 'https://google.com',
-    github: 'https://github.com',
-    techs: ['Next.js', 'Tailwind CSS', 'TypeScript'],
+    link: 'https://mshahanwaz.vercel.app/',
+    github: 'https://github.com/mshahanwaz/portfolio-v4',
+  },
+  {
+    title: 'Amazon Clone',
+    description:
+      'Fully functional Amazon website clone with user authentication, payment gateway, and basket functionality.',
+    image: [
+      'https://images.unsplash.com/photo-1508919801845-fc2ae1bc2a28?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1nfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+      'https://images.unsplash.com/photo-1508919801845-fc2ae1bc2a28?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1nfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+      'https://images.unsplash.com/photo-1508919801845-fc2ae1bc2a28?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1nfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+    ],
+    link: 'https://amazon-clone-mshahanwaz.vercel.app/',
+    github: 'https://github.com/mshahanwaz/amazon-build',
+  },
+  {
+    title: 'Image Processing in C++',
+    description:
+      'A terminal based image processing tool written in C++ to perform various image processing operations on .bmp file format.',
+    image: [
+      'https://images.unsplash.com/photo-1508919801845-fc2ae1bc2a28?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1nfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+      'https://images.unsplash.com/photo-1508919801845-fc2ae1bc2a28?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1nfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+      'https://images.unsplash.com/photo-1508919801845-fc2ae1bc2a28?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1nfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+    ],
+    github: 'https://github.com/mshahanwaz/ip_cpp_2022',
   },
 ];
 
@@ -71,25 +92,29 @@ export default function Projects() {
                     />
                   </div>
                   <div className="order-4 flex flex-col gap-4 rounded-lg bg-gray-100 p-4 dark:bg-gray-800 lg:order-2 lg:col-span-4 lg:row-span-3 lg:gap-2">
-                    <h2 className="w-fit text-3xl font-bold text-gray-900 hover:bg-gray-200 dark:text-gray-50 dark:hover:bg-gray-700">
-                      <Link href={`/projects/${projectIndex}`}>
-                        {project.title}
-                      </Link>
+                    <h2 className="w-fit text-3xl font-bold text-gray-900 dark:text-gray-50">
+                      {project.title}
                     </h2>
-                    <p className="text-base">{project.brief}</p>
+                    <p className="text-base">{project.description}</p>
                     <div className="mt-auto flex flex-wrap gap-2 text-sm font-medium">
-                      <Link
-                        href={project.link}
-                        className="flex items-center gap-2 rounded-full bg-gray-200 px-3 py-1 hover:bg-gray-300 hover:text-gray-900 dark:bg-gray-700 dark:hover:bg-gray-600 dark:hover:text-gray-50"
-                      >
-                        <span>Link</span>
-                        <span>
-                          <ArrowUpRightIcon className="h-4 w-4" />
-                        </span>
-                      </Link>
+                      {project?.link && (
+                        <Link
+                          href={project.link}
+                          className="flex items-center gap-2 rounded-full bg-gray-200 px-3 py-1 hover:bg-gray-300 hover:text-gray-900 dark:bg-gray-700 dark:hover:bg-gray-600 dark:hover:text-gray-50"
+                          target="_blank"
+                          rel="noreferrer noopener"
+                        >
+                          <span>Link</span>
+                          <span>
+                            <ArrowUpRightIcon className="h-4 w-4" />
+                          </span>
+                        </Link>
+                      )}
                       <Link
                         href={project.github}
                         className="flex items-center gap-2 rounded-full bg-gray-200 px-4 py-1 hover:bg-gray-300 hover:text-gray-900 dark:bg-gray-700 dark:hover:bg-gray-600 dark:hover:text-gray-50"
+                        target="_blank"
+                        rel="noreferrer noopener"
                       >
                         <span>Github</span>
                         <span>
